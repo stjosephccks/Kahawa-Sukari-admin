c2d30fbef85a09f9f51bfab324b047242001bf60
frankesomcare <frankline.wakasiaka@esomcare.com>
2023-12-14 23:55:46 +0300

    font size

--- components/RichTextEditor.js
+++ components/RichTextEditor.js
@@ -2,6 +2,7 @@ import dynamic from 'next/dynamic';
 import { useEffect, useState } from 'react';
 import { ContentState, EditorState, convertFromHTML, convertFromRaw, convertToRaw } from 'draft-js';
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
+import { FaBeer } from "react-icons/fa";
  const Editor = dynamic(
     ()=> import("react-draft-wysiwyg").then((module)=>module.Editor),
     {
@@ -35,13 +36,19 @@ export default function RichTextEditor({ onEditorStateChange, initialContent }){
                     toolbarClassName=" text-md flex sticky-top-0 z-50  !justify-center mx-auto"
                     editorClassName='mt-6 p-10  bg-white min-h-screen shadow-lg max-w-4xl mx-auto mb-12 border'
                     toolbar={{
-                        options: ['inline', 'blockType', 'list', 'textAlign'],
+                        options: ['inline', 'blockType','fontSize', 'list', 'textAlign'],
                         inline: {
                           options: ['bold', 'italic', 'underline'],
                         },
                         blockType: {
                           inDropdown: true,
                           options: ['Normal', 'H1', 'H2', 'H3', 'Blockquote'],
+                        },
+                        fontSize: {
+                          icon: <FaBeer />,
+                          inDropdown:true,
+                          options: [8, 9, 10, 11, 12, 14, 16, 18, 24, 30, 36, 48, 60, 72, 96],
+                          
                         },
                         list: {
                           options: ['unordered', 'ordered'],
